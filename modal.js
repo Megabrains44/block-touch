a660c52eea19802188410d4f4a8cb56d712b1929
Megabrains44 <75341249+Megabrains44@users.noreply.github.com>
2022-05-01 15:40:01 -0500

    fixed glitch

--- modal.js
+++ modal.js
@@ -2,9 +2,11 @@ const infoBtn = document.getElementById("info-btn");
 const modalBG = document.getElementById("modal-bg");
 const xButton = document.querySelector("#modal .x-btn")
 infoBtn.addEventListener("click", e => {
-    modalBG.classList.toggle('bg-hide')
+    modalBG.style.visibility = "visible";
+    modalBG.style.opacity = "1";
 })
 
 xButton.addEventListener("click", e => {
-    modalBG.classList.toggle('bg-hide')
+    modalBG.style.visibility = "hidden";
+    modalBG.style.opacity = "0";
 })
